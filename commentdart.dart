19745e1940d0d5e146c3018b72f8e39e78ad6c4e
natinael <natnaelmasresha2022@gmail.com>
2024-05-30 17:35:02 +0300

    dart comment styles options

---
+++ commentdart.dart
@@ -0,0 +1,16 @@
+
+
+
+void main(){
+	// IN - line comment
+		// this is inline comment of code
+	/*
+		block comment
+		multiple line of comment
+	*/
+	
+	/// Documentation 
+		/// This is comment type used to specify documentation part of the code 
+
+
+}
