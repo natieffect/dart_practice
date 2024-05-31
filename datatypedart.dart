26d6205c3946f874ebcaf9620250aea5a6d63633
natinael <natnaelmasresha2022@gmail.com>
2024-05-31 12:09:50 +0300

    dart data types and initiation

---
+++ datatypedart.dart
@@ -0,0 +1,28 @@
+
+
+void main(){
+	// data types in dart
+		/*
+			1, int
+			2, double
+			3, String
+			4, bool
+			5, dynamic
+		*/
+
+	// int data types 
+		int frsintdart  = 2;
+	// double data types
+		double frsdbldart = 0.1;
+	// bool data typesi
+		bool frsboldart = false;
+	// dynamic data types
+		dynamic frsdncdart =  100;
+		dynamic frtdyncdart = frsintdart + frsintdart + frsdbldart;
+
+	print(frtdyncdart);
+
+
+
+
+}
