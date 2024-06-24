c81515ae746bd6f463fa9a5336d969d814849f8d
natinael <natnaelmasresha2022@gmail.com>
2024-06-24 20:09:01 +0300

    dart iterations

---
+++ iterationdart.dart
@@ -0,0 +1,15 @@
+/// Iteration
+
+	// in dart to iterate object properties we use
+	// for in loop works 
+
+
+void main(){
+
+	var varfrs = [1,2,4,6,7,8];
+	for(int pptfrs in varfrs){
+		print(pptfrs);
+	}
+
+
+}
