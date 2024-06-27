9ef3d916443ded87f6a4ba48185ae7fc0926df24
natinael <natnaelmasresha2022@gmail.com>
2024-06-27 20:00:11 +0300

    tested git artimethic opetations

---
+++ operatorArtimeticdart.dart
@@ -0,0 +1,56 @@
+
+/// Arthimethic operator 
+	// the equality and relation operators of both are 
+	// almost identical as js
+
+void main(){
+
+/// [A] Addtion 
+	var varfrs =  10;
+	var varsec =  0.20;
+	print(varfrs+varsec);
+
+///[B] Substraction 
+	var vartrd = 50;
+	var varfor = 0.90;
+	print(vartrd - varfor);
+
+///[C]. Unary minus (Negation)
+	var varfiv = 40;
+	var varsix = -(varfiv);
+	print(varsix);
+
+///[D]. Multiply
+	var varsvn = 60;
+	var varegt = 30;
+	print(varsvn * varegt);
+
+///[E], Divide
+	var varnin = 9;
+	var varten = 10;	
+	print(varnin/varten);
+
+///[F]. Divide returning an integer result ( Truncating division)
+	// divide double and output floored integer
+	var varelv = 0.100;
+	var vartwl =  varelv ~/ 20;
+	print(vartwl);
+
+///[G]. Modulo
+	var varthn = 13;
+	var varftn = 14;
+	print(varftn % varthn);
+
+///[H]. prefix  in dart (++D/--D)
+	var varsin = 16;
+	var varstn = ++varsin;
+	print(varstn);
+
+///[I]. postfix (D--/ D++)
+	var varetn = varstn++;
+	print(varetn);
+	
+
+
+
+}
