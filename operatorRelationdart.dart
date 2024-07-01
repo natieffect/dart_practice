b724fc313c54c711b4e00bef9f021efffa5771bf
natinael <natnaelmasresha2022@gmail.com>
2024-07-01 09:44:18 +0300

    tested dart relational operations

---
+++ operatorRelationdart.dart
@@ -0,0 +1,31 @@
+
+/// Relational operator
+// the equality and relational operators of both
+
+
+void main(){
+
+	//[A] strict equal
+	var varfrs = 'firstvar';
+	var varsec = 'secondvar';
+	varfrs == varsec? print(true):print(false);
+
+	//[B] Strict not equal
+	var vartrd = 21;
+	var varfor = 23;
+	vartrd != varfor? print(true):print(false);
+	
+	//[C] Greater than 
+	var varfiv = 10;
+	var varsix = 21;
+	varfiv > varsix ? print(true):print(false);
+	
+	//[D] Less than 
+	var varsvn = 7;
+	var varegt = 8;
+	varsvn < varegt ? print(true):print(false);	
+	//[E] less than or equal to
+	var varnin = 9;
+	var varten = 10;
+	varnin <= varten ? print(true):print(false);
+}
