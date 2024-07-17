9b8b271682d15cbcadc76dfe1b95b675c92f43d6
natinael <natnaelmasresha2022@gmail.com>
2024-07-17 12:45:01 +0300

    tested dart bit wise xor operator functions uses and declaritons

---
+++ bitwisexordart.dart
@@ -0,0 +1,17 @@
+
+// bit wise XOR
+	
+void main(){
+
+	// ^ (XOR)
+	print('testing the bit wise x or values ');
+	print('returns one at the place have d/f but values');
+	var varfrs = 2 ^ 3;
+	// 10 ^ 11 -> 01 = 1
+	print(varfrs);
+
+	var varsec =  4 ^ 3;
+	// 100 ^ 11 -> 111 = 7
+	print(varsec);
+
+}
