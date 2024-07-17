c9179002efb16ba34d4f61cdb7f9be74e0a3ab56
natinael <natnaelmasresha2022@gmail.com>
2024-07-17 12:53:58 +0300

    tested dart assignment operator

---
+++ assignmentdart.dart
@@ -0,0 +1,82 @@
+
+ /// Assignments
+	// values update the current state and assign the result back to the variable 
+
+	// Types
+	 //[1] = 
+	 //[2] *=
+	 //[3] /=
+	 //[4] +=
+	 //[5] -=
+	 //[6] &=
+	 //[7] ^=
+	 //[8] %=
+	 //[9] >>>=
+	 //[10] <<=
+	 //[11] |=
+	 //[12] ~/=
+	 //[13] >>=
+
+
+void main(){
+
+	//[1] =
+	print('testing = (assignment operator)');
+	var varfrs = 3;
+	print(varfrs);
+
+	//[2] *=
+	print('testing *= operator');
+	varfrs *=3;
+	print(varfrs);
+
+	//[3] /= 
+	print('testing /= operator');
+	double vartrd = 4;
+	double varfor = 5;
+	vartrd /= varfor;
+	 print(vartrd);
+
+	//[4] +=
+	print('testing += opertor');
+	var varsix = 6;
+	print(varsix +=6);
+
+	//[7] -=
+	print('testing -=');
+	var varsvn = 7;
+	print(varsvn -=3);
+
+	//[8] %=
+	print('tesging %=');
+	var varegt = 8;
+	print(varegt %=2);
+
+	//[9] >>>=
+	print('testing >>>= ');
+	var varnin = 9;
+	print(varnin >>>=2);
+
+	//[10] <<=
+	print('testing value by <<=');
+	var varten  = 10;
+	print(varten <<=2);
+
+	//[11]  |=
+	print('testing |= operator');
+	print('performs bit wise or operation ');
+	var vareln = 11;
+	print(vareln |= 2);
+
+	//[12]  ~/= 
+	print('testing ~/= operation in dart');
+	var vartlv = 12;
+	print(vartlv ~/5);
+
+	//[13] >>=
+	print('testing the operator >>=');
+	var vartrn = 13;
+	print(vartrn >>=2);
+
+
+}
