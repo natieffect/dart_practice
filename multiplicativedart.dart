c4ad69b27c13a19210715e638b7acd5e9dd01ac5
natinael <natnaelmasresha2022@gmail.com>
2024-07-17 18:05:17 +0300

    testing dart multiplication opetatiors types

---
+++ multiplicativedart.dart
@@ -0,0 +1,34 @@
+
+/// Multiplicative 
+
+	// type includes 
+		// [1] *
+		// [2] /
+		// [3] %
+		// [4] ~/
+
+
+void main(){
+
+	//[1] *
+	print('testing * multiplication ');
+	var varfrs = 5 * 6;
+	print(varfrs);
+
+	//[2] /
+	print('testing divistion / ');
+	var varsec = 4/2;
+	print(varsec);
+
+	//[3] % 
+	print('testing module');
+	var vartrd = 6%3;
+	print(vartrd);
+
+	//[4] ~/
+	print('testing the division ');
+	print('returns integer result agter divisuion');
+	var varfor = 5~/2;
+	print(varfor);
+
+}
