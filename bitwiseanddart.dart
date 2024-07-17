06ed70bdb6abd0da9bcfe29f63101b293eef8c0b
natinael <natnaelmasresha2022@gmail.com>
2024-07-17 12:43:49 +0300

    tested dart bit wise and operator details

---
+++ bitwiseanddart.dart
@@ -0,0 +1,14 @@
+/// Bit wise and 
+
+void main(){
+
+	print('testing bit wise and value ');
+	print(' returns one at every bit where both the operands have one');
+	var varfrs = 2 & 2;
+	// 2 -> 10 + 10 = 10	
+	var varsec = 2 & 3;
+	// 10 + 11 -> 10
+	print(varsec);
+
+
+}
